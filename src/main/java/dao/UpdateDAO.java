517de201e256b2ba675977975f84ac7e1c56d168
danielfurtadobezerra <danielfurtadobezerra@gmail.com>
2022-11-25 18:59:05 -0300

    Timer/Schedule

--- src/main/java/dao/UpdateDAO.java
+++ src/main/java/dao/UpdateDAO.java
@@ -1,21 +1,36 @@
 package dao;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import configDB.Conexao;
 import entidade.UpdadeVo;
 
 public class UpdateDAO {
-
-	public void inserirDados() {
-		Conexao c = new Conexao("localhost","5432","restUpdate","admin","admin");
-		c.conect();
-		UpdadeVo up = new UpdadeVo();
-		String sql = "insert into update (id, idUpdate, currentR eleaseDate, cvrfUrl, documentTitle, initialReleaseDate, severity)"+
-					  "values (default,"+up.getIdUpdate()+", "+up.getCurrentReleaseDate()+", "+up.getCvrfUrl()+", "+up.getDocumentTitle()+", "+up.getInitialReleaseDate()+", "+up.isSeverity()+")";
-		int res = c.executaSQL(sql);
-		c.disconect();
-		if (res == 0) {
-			System.out.println("Erro");
-		}
-	}
 	
+	ScheduledExecutorService scheduledExecutorService =
+	        Executors.newScheduledThreadPool(1);
+	
+	@SuppressWarnings("rawtypes")
+	ScheduledFuture scheduledFuture =
+		    scheduledExecutorService.schedule(new Callable() {
+		    	public void inserirDados() {
+		    		Conexao c = new Conexao("localhost","5432","restUpdate","admin","admin");
+		    		c.conect();
+		    		UpdadeVo up = new UpdadeVo();
+		    		String sql = "insert into update (id, idUpdate, currentR eleaseDate, cvrfUrl, documentTitle, initialReleaseDate, severity)"+
+		    					  "values (default,"+up.getIdUpdate()+", "+up.getCurrentReleaseDate()+", "+up.getCvrfUrl()+", "+up.getDocumentTitle()+", "+up.getInitialReleaseDate()+", "+up.isSeverity()+")";
+		    		int res = c.executaSQL(sql);
+		    		c.disconect();
+		    		if (res == 0) {
+		    			System.out.println("Erro");
+		    		}
+		    	}
+		    },
+		    300,
+		    TimeUnit.SECONDS);
+}
 }
